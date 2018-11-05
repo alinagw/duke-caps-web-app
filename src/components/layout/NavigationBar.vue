baa9b7d2174685b9809e009818b581b1a43d0edc
Alina Walling <alina.walling@duke.edu>
2018-11-05 16:29:28 -0500

    changed the webpack project to one with bootstrap vue

--- src/components/layout/NavigationBar.vue
+++ src/components/layout/NavigationBar.vue
@@ -1,16 +1,42 @@
 <template>
-  <div>
-  </div>
+<b-navbar toggleable="md" type="dark" variant="info">
+
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item>
+                <router-link to="/">Home</router-link>
+            </b-nav-item>
+            <b-nav-item>
+                <router-link to="/about-us">About Us</router-link>
+            </b-nav-item>
+            <b-nav-item>
+                <router-link to="/get-started">Get Started</router-link>
+            </b-nav-item>
+            <b-nav-item>
+                <router-link to="/services">Our Services</router-link>
+            </b-nav-item>
+            <b-nav-item>
+                <router-link to="/resources">Resources</router-link>
+            </b-nav-item>
+        </b-navbar-nav>
+    </b-collapse>
+</b-navbar>
 </template>
 
 <script>
 export default {
-  name: 'NavigationBar',
-  data () {
-    return {
-      
+    name: 'NavigationBar',
+    data() {
+        return {
+
+        }
     }
-  }
 }
 </script>
 
