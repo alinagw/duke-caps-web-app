382501fa72ad6ec14530fed7380ab9735ee0d240
Andres Sebastian Hernandez Guerra <36876416+AndresHernandezGuerra@users.noreply.github.com>
2018-11-13 17:49:17 -0500

    Added Vue Pages for the Articles, and Scrollspy
    Restructured some pages for better layout, and also added scrollspy to them!

--- src/components/pages/article-pages/ArticleLivingLifeValues.vue
+++ src/components/pages/article-pages/ArticleLivingLifeValues.vue
@@ -1,19 +1,33 @@
 <template>
-<div>
-    <h1>About Us</h1>
-    <p>
-        CAPS helps Duke Students enhance strengths and develop abilities to successfully live, grow and learn in their personal and academic lives. We offer many services to Duke undergraduate, graduate, and professional students, including brief individual and group counseling, couples counseling and more. CAPS staff also provide outreach to student groups, particularly programs supportive of at-risk populations, on a wide range of issues impacting them in various aspects of campus life.
-    </p>
-    <p>
-        CAPS is designed as a short-term care clinic. Therefore, we are limited in our ability to provide care for students needing long-term or more open-ended care.
-    </p>
-    <p>
-        There are certain specialized services such as ADD/ADHD and treatment for Substance Use Disorders that are not available through CAPS.
-    </p>
-    <p>
-        We do recognize that several our students will need longer term or a more specialized type of care and we have a strong network with area community providers for these.
-    </p>
-</div>
+<b-row no-gutters>
+    <b-col cols="3">
+        <b-list-group v-scroll-spy-active v-scroll-spy-link tag="ul" class="article-scroll-menu">
+            <b-list-group-item v-for="(title, index) in titles" :key="index" tag="li" class="article-scroll-menu-item">
+                <a>{{ title }}</a>
+            </b-list-group-item>
+        </b-list-group>
+    </b-col>
+    <b-col cols="9">
+        <div v-scroll-spy="{data: 'section', offset: 80}" class="article-scroll-content">
+            <div>
+               <h1>LIVING YOUR LIFE-VALUES</h1>
+            </div>
+            <div>
+               <h4>Your Map for Living</h4>
+               <p>What guides you? What are your personal priorities that influence the decisions you make in your day-to-day life?</p>
+               <p>Your personal priorities doesn't refer to your task list. It doesn't refer to the things you have to do, necessarily, such as study for an exam, work on a paper, or complete that task for the extra-curricular organization you lead. Your personal priorities are the larger truths that help you define any task as important to begin with.</p>
+               <p>In other words, what are the things in life that you value enough that you want these values to guide your living?</p>
+               <p>What we tend to see is that if you have a clearer sense of what your values area your personal priorities that clarity helps you live with a higher level of self-esteem, with a greater sense of fulfillment, and for the times that life becomes more challenging, a greater source of resilience.</p>
+               <p>There are a number of ways to organize your personal values system and an almost infinite number of ways to define the different values. Regardless of how you might do that, it's important to consider three general steps in using values or personal priorities to guide your life:</p>
+               <p>First, it is important to identify what matters to you and what are the terms or categories of this so you can have some language to explore your values. In other words, what would go on the list of your values, and how would you prioritize them?</p>
+               <p>Second, it is important to explore the way you're actually living your life; how the decisions you make and the patterns of your investments reflect what is most meaningful to you. This is important because a flourishing life is one beyond mere awareness of one's values; a congruent expression of those things that are most important to you plays a big role in how satisfied you are with your life, how resilient you are in facing challenges, and how meaningful your life experiences are.</p>
+               <p>Finally, it becomes important to assess what changes you may want to make in how you devote your time, energy, and creativity so that your day-to-day living is more in line with what you recognize as your</p>
+               <p>personal priorities. Sometimes, this leads you to realize that your priorities have changed without you noticing, and that's okay. It simply cycles you back to the first step as you move through your life, continually discovering what matters most to you and how to make that your map for living.</p>
+               <p><a href="https://www.lifevaluesinventory.org/" target="_blank">The Life Values Inventory</a> is one tool that has helped many people go through this process.</p>
+            </div>
+        </div>
+    </b-col>
+</b-row>
 </template>
 
 <script>
@@ -23,9 +37,13 @@ export default {
 
     },
     data() {
-        return {
-
-        }
+            return {
+                section: 0,
+                titles: [
+                    'Living Your Life Values',
+                    'Your Map For Living',
+                ]
+            }
     }
 }
 </script>
