f4dac5fe4b976898c7bcbee98612305acea86dda
Andres Sebastian Hernandez Guerra <36876416+AndresHernandezGuerra@users.noreply.github.com>
2018-11-13 23:18:40 -0500

    Vue Pages and New Layout for Subpage

--- src/components/pages/article-pages/ArticleDisorderedEating.vue
+++ src/components/pages/article-pages/ArticleDisorderedEating.vue
@@ -1,19 +1,16 @@
 <template>
-<div>
-    <h1>About Us</h1>
-    <p>
-        CAPS helps Duke Students enhance strengths and develop abilities to successfully live, grow and learn in their personal and academic lives. We offer many services to Duke undergraduate, graduate, and professional students, including brief individual and group counseling, couples counseling and more. CAPS staff also provide outreach to student groups, particularly programs supportive of at-risk populations, on a wide range of issues impacting them in various aspects of campus life.
-    </p>
-    <p>
-        CAPS is designed as a short-term care clinic. Therefore, we are limited in our ability to provide care for students needing long-term or more open-ended care.
-    </p>
-    <p>
-        There are certain specialized services such as ADD/ADHD and treatment for Substance Use Disorders that are not available through CAPS.
-    </p>
-    <p>
-        We do recognize that several our students will need longer term or a more specialized type of care and we have a strong network with area community providers for these.
-    </p>
-</div>
+    <div>
+        <div>
+            <h1>Disordered Eating</h1>
+            <p>Struggles with eating issues are are about more than just food, though if you are suffering from these issues you may not be able to explain what else is happening. What you do know is that you struggle a lot more than you want to with eating or not eating, gaining weight, keeping your current weight, or losing weight. All this holds so much of your attention that it's hard to get to what these eating issues are really about. So what is this "disordered eating" all about, then?</p>
+        </div>
+        <div class="d-flex flex-row flex-wrap justify-content-center">
+            <b-btn href="#" target="_blank">Terms and Diagnosis</b-btn>
+            <b-btn href="#" target="_blank">Understanding Disordered Eating</b-btn>
+            <b-btn href="#" target="_blank">How To Help?</b-btn>
+            <b-btn href="#" target="_blank">What To Say?</b-btn>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -31,5 +28,33 @@ export default {
 </script>
 
 <style scoped>
+.btn {
+    margin: 24px auto;
+    background-color: #38C6D4;
+    color: white;
+    font-family: "Montserrat", "sans-serif";
+    font-size: 14px;
+    letter-spacing: 1px;
+    padding: 12px 16px;
+    border: none;
+    border-radius: 8px;
+    transition: 0.3s;
+    -webkit-box-shadow: 0px 2px 5px 0px rgba(69, 91, 99, 0.45);
+    -moz-box-shadow: 0px 2px 6px 0px rgba(69, 91, 99, 0.45);
+    box-shadow: 0px 2px 5px 0px rgba(69, 91, 99, 0.45);
+}
 
+.btn:hover,
+.btn:focus {
+    -webkit-box-shadow: 0px 2px 10px 0px rgba(69, 91, 99, 0.45);
+    -moz-box-shadow: 0px 2px 10px 0px rgba(69, 91, 99, 0.45);
+    box-shadow: 0px 2px 10px 0px rgba(69, 91, 99, 0.45);
+    background-color: #1f7998 !important;
+}
+
+.btn:hover {
+    -webkit-box-shadow: 0px 5px 18px 0px rgba(69, 91, 99, 0.4);
+    -moz-box-shadow: 0px 5px 18px 0px rgba(69, 91, 99, 0.4);
+    box-shadow: 0px 5px 18px 0px rgba(69, 91, 99, 0.4);
+}
 </style>
