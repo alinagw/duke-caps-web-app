8f82da62ed1786fda3ea37027324cf88b802781e
Alina Walling <alina.walling@duke.edu>
2018-11-14 06:31:54 -0500

    :((

--- dist/static/js/manifest.3db4238b1be122054ac0.js
+++ dist/static/js/manifest.3db4238b1be122054ac0.js
@@ -141,11 +141,11 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
 /******/ })
 /************************************************************************/
 /******/ ([]);
-//# sourceMappingURL=manifest.108f67684a07feafbdeb.js.map
+//# sourceMappingURL=manifest.3db4238b1be122054ac0.js.map
